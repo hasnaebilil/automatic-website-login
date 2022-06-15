8112606f6ef7573af697bf52c4554d8197090e1b
Hannah <hasnae.bilil@gmail.com>
2022-06-15 16:48:18 -0400

    Udacity example - Angular

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from selenium import webdriver
 username = "your email"
 password = "your password"
 
-url = "https://github.com/login"
+url = "https://auth.udacity.com/sign-in"
 
 driver = webdriver.Chrome("C:/Users/hasna/Downloads/chromedriver")
 
 driver.get(url)
 
-driver.find_element_by_id("login_field").send_keys(username)
-driver.find_element_by_id("password").send_keys(password)
-driver.find_element_by_name("commit").click()
+driver.find_element_by_id("email").send_keys(username)
+driver.find_element_by_id("revealable-password").send_keys(password)
+driver.find_element_by_css_selector(".vds-form-fieldset>:last-child>:last-child").click()
 
 
 print("logged in sucessefully")
